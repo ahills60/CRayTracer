a32ebd00708447496739a1dd2116826f99497a05
Andrew Hills <ahills60@gmail.com>
2013-12-06 12:56:34 +0000

    Added more colours
    Further colours added to colours.h

--- colours.h
+++ colours.h
@@ -15,6 +15,11 @@
 int RED[3] = {255, 0, 0}; // You may need to convert to FP
 int GREEN[3] = {0, 255, 0};
 int BLUE[3] = {0, 0, 255};
-
+int WHITE[3] = {255, 255, 255};
+int BLACK[3] = {0, 0, 0};
+int PURPLE[3] = {140, 0, 255};
+int YELLOW[3] = {255, 255, 0};
+int BROWN[3] = {148, 255, 0};
+int CYAN[3] = {0, 255, 255};
 
 #endif /* COLOURS_H_ */
