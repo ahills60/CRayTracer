32a487d3b2ad1898a05bec7737138a71fe753fca
Andrew Hills <ahills60@gmail.com>
2013-12-10 16:25:53 +0000

    Example created with construct.h
    Construct.h builds a scene with two objects surrounded by walls, each
    of garish colours.

---
+++ construct.h
@@ -0,0 +1,112 @@
+/*
+ * construct.h
+ * 
+ * Builds a scene with the primitives created in other header files
+ *
+ *  Created on: 6 Dec 2013
+ *      Author: andrew
+ */
+
+#ifndef CONSTRUCT_H_
+#define CONSTRUCT_H_
+
+#include "craytracer.h"
+#include "datatypes.h"
+#include "rays.h"
+#include "image.h"
+#include "lighting.h"
+#include "objects.h"
+#include "colours.h"
+#include "shapes.h"
+
+/* Populate a scene with set items */
+void populateScene(Scene *scene)
+{
+    Object cube, planeBase, planeLeft, planeRight, planeTop, planeBack, mirrCube;
+    Material redGlass, nonreflBlue, nonreflGreen, nonreflPurple, mirror;
+    Vector red = int2Vector(RED);
+    Vector blue = int2Vector(BLUE);
+    Vector green = int2Vector(GREEN);
+    Vector purple = int2Vector(PURPLE);
+    Vector white = int2Vector(WHITE);
+    
+    // Set material types
+    setMaterial(&redGlass, red, 0.0, 0.5, 0.0, 0.0, 0.0, 0.8, 1.4);
+    setMaterial(&nonreflBlue, blue, 0.1, 0.5, 0.4, 2.0, 0.0, 0.0, 1.4);
+    setMaterial(&nonreflGreen, green, 0.1, 0.5, 0.4, 2.0, 0.0, 0.0, 1.4);
+    setMaterial(&nonreflPurple, purple, 0.1, 0.5, 0.4, 2.0, 0.0, 0.0, 1.4);
+    setMaterial(&mirror, white, 0.1, 0.0, 0.9, 32.0, 0.6, 0.0, 1.4);
+    
+    // Create objects
+    createCube(&cube, redGlass, 1.0);
+    createPlaneXZ(&planeBase, nonreflPurple, 10.0);
+    createPlaneXZ(&planeTop, nonreflPurple, 10.0);
+    createPlaneXZ(&planeLeft, nonreflGreen, 10.0);
+    createPlaneXZ(&planeRight, nonreflGreen, 10.0);
+    createPlaneXZ(&planeBack, nonreflBlue, 10.0);
+    createCube(&mirrCube, mirror, 1.5);
+    
+    // Arrange
+    transformObject(&cube, matMult(genTransMatrix(2, 0.5, -1), genYRotateMat(45)));
+    transformObject(&planeBase, genTransMatrix(1, 0, -4));
+    transformObject(&planeLeft, matMult(genTransMatrix(-2, 0, -4), genZRotateMat(-90)));
+    transformObject(&planeRight, matMult(genTransMatrix(4, 0, -4), genZRotateMat(90)));
+    transformObject(&planeBack, matMult(genTransMatrix(1, 0, -6), genXRotateMat(90)));
+    transformObject(&planeTop, matMult(genTransMatrix(1, 5, -4), genZRotateMat(180)));
+    transformObject(&mirrCube, matMult(genTransMatrix(0, 0.9, -2.7), genYRotateMat(20)));
+    
+    // Create the scene
+    initialiseScene(scene, 6);
+    addObject(scene, cube);
+    addObject(scene, planeBase);
+    addObject(scene, planeLeft);
+    addObject(scene, planeRight);
+    addObject(scene, planeBack);
+    addObject(scene, mirrCube);
+    // addObject(scene, planeTop);
+}
+
+Vector draw(Ray ray, Scene scene, Light light, int recursion)
+{
+    Hit hit;
+    Vector outputColour, reflectiveColour, refractiveColour;
+    float reflection, refraction;
+    
+    // Default is black. We can add to this (if there's a hit) 
+    // or just return it (if there's no object)
+    setVector(&outputColour, 0, 0, 0);
+    
+    hit = sceneIntersection(ray, scene);
+    
+    // Determine whether there was a hit. Otherwise default.
+    if (hit.objectIndex >= 0)
+    {
+        // There was a hit.
+        
+        outputColour = vecAdd(ambiance(hit, scene, light), vecAdd(diffusion(hit, scene, light), specular(hit, scene, light)));
+        
+        // Should we go deeper?
+        if (recursion > 0)
+        {
+            // Yes, we should
+            // Get the reflection
+            reflectiveColour = draw(reflectRay(hit), scene, light, recursion - 1);
+            reflection = scene.object[hit.objectIndex].material.reflectivity;
+            outputColour = vecAdd(outputColour, scalarVecMult(reflection, reflectiveColour));
+            
+            // Get the refraction
+            refractiveColour = draw(refractRay(hit, scene.object[hit.objectIndex].material.refractivity), scene, light, recursion - 1);
+            refraction = scene.object[hit.objectIndex].material.opacity;
+            outputColour = vecAdd(outputColour, scalarVecMult(refraction, refractiveColour));
+        }
+        
+        // We've got what we needed after the hit, so return
+        return scalarVecMult(1.0 - traceShadow(hit, scene, light), outputColour);
+    }
+    
+    // No hit, return black.
+    
+    return outputColour;
+}
+
+#endif
