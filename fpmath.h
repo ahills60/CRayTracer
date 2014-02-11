028a9ea4ca7e17f5e5d144c9368766a2b1021242
Andrew Hills <ahills60@gmail.com>
2014-02-11 14:08:55 +0000

    Fixes fixed point multiplication
    - Range checking has been fixed within fixed point multiplication
    function.

--- fpmath.h
+++ fpmath.h
@@ -1,6 +1,7 @@
 /*
  *  fpmath.h
- *  Provides fixed point math functionality
+ *  Provides fixed point math functionality.
+ *  Assumes 16:16 fixed point notation.
  *
  *
  *  Created on: 11 Feb 2014
@@ -21,8 +22,8 @@ fixedp fp_mult(fixedp a, fixedp b)
     int64 result = ((int64) a) * ((int64) b) / (int64)(1<<16);
     
 #ifdef CHECK_RANGE
-    if (r > (long long int) 1 << 31)
-        printf("Overflow in downcast during mul %lld, %f\n", r, r/65535.0);
+    if (result > (long long int) 1 << 31)
+        printf("Overflow in downcast during mul %lld, %f\n", result, result/65535.0);
 #endif
     
     return (int)(result);
