e3eed274b6b466d55e4e45eb5828f8090ac9121b
Andrew Hills <ahills60@gmail.com>
2014-03-18 14:40:19 +0000

    Implements basic functionality within the interactive header file
    - The interactive header file now contains the basic structure to deal
    with GLUT output.
    - Adds skeleton functions for handling keyboard input.

--- interactive.h
+++ interactive.h
@@ -12,10 +12,77 @@
 
 #include <GL/glut.h>
 
+// Make a variable for storing pixels accessible to all functions
+unsigned int *PixelStore;
+unsigned int ScreenWidth, ScreenHeight;
+
+/* Initialise pixel store */
+void initialisePixelStore(int width, int height)
+{
+    // Ensure we have enough memory to store pixel information
+    PixelStore = (unsigned int *)malloc(sizeof(unsigned int) * width * height);
+    // Finally wipe this space
+    memset(PixelStore, 0, sizeof(unsigned int) * width * height);
+}
+
+/* Quick function to wipe the pixel store */
+void clearPixelStore()
+{
+    // A simple wipe of the memory location.
+    memset(PixelStore, 0, sizeof(unsigned int) * width * height);
+}
+
+/* Function to define window resizing */
+void reshapeFunc(int newWidth, int newHeight)
+{
+    // We don't allow this to be resized. Change it back!
+    glutReshapeWindow(ScreenWidth, ScreenHeight);
+}
+
+/* Function to define idle time tasks */
+void idleFunc(void){
+    glutPostRedisplay();
+}
+
+/* Function to take control of user input elements */
+void keyboardFunc(unsigned char key, int xmouse, int ymouse)
+{
+    switch(key)
+    {
+        case 'Q':
+        case 'q':
+            break;
+        default:
+            break;
+    }
+}
+
+/* Function to handle special key input */
+void specialFunc(int key, int x, int y)
+{
+    switch(key)
+    {
+        default:
+            break;
+    }
+}
+
+/* Function to handle what's displayed within the window */
+void displayFunc(void)
+{
+    
+}
+
 /* Function to initialise GLUT window and output */
 void initialiseGLUT(int width, int height)
 {
     glutInitWindowSize(width, height);
+    
+    // Store the size into the global variable
+    ScreenWidth = width;
+    ScreenHeight = height;
+    
+    // Back to window setup
     glutInitWindowsPosition(0, 0);
     glutInitDisplayMode(GLUT_RGBA | GLUT_ALPHA | GLUT_DOUBLE);
     
@@ -26,6 +93,7 @@ void initialiseGLUT(int width, int height)
     glutDisplayFunc(displayFunc);
     glutIdleFunc(idleFunc);
     glutKeyboardFunc(keyboardFunc);
+    glutSpecialFunc(specialFunc);
     glutReshapeFunc(reshapeFunc);
     
     glViewport(0, 0, width, height);
